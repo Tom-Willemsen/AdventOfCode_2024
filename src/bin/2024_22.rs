adf7d899e989f71b321be90dc906121f73530020
Tom Willemsen <tomwillemsen1995@gmail.com>
2024-12-22 14:27:13 +0000

    2024-22

--- src/bin/2024_22.rs
+++ src/bin/2024_22.rs
@@ -1,8 +1,13 @@
 #![cfg_attr(feature = "bench", feature(test))]
 use advent_of_code_2024::{Cli, Parser};
-use itertools::{repeat_n, Itertools};
+use ahash::HashMapExt;
+use mimalloc::MiMalloc;
 use rayon::prelude::*;
-use std::{collections::VecDeque, fs};
+use rustc_hash::FxHashMap;
+use std::fs;
+
+#[global_allocator]
+static GLOBAL_ALLOC: MiMalloc = MiMalloc;
 
 fn next_secret(mut n: i64) -> i64 {
     n = (n ^ (n << 6)) & 0xFFFFFF;
@@ -11,88 +16,48 @@ fn next_secret(mut n: i64) -> i64 {
     n
 }
 
-fn ones_digit(n: i64) -> i64 {
-    n % 10
-}
-
-fn sell(n: i64, c1: i64, c2: i64, c3: i64, c4: i64) -> Option<i64> {
-    let mut diffs = VecDeque::default();
+fn sell(n: i64) -> (i64, FxHashMap<u32, i64>) {
     let mut n = n;
-    for _ in 0..2000 {
+    let mut map = FxHashMap::with_capacity(2000);
+
+    let mut diffs: u32 = 0;
+
+    for i in 0..2000 {
         let next_n = next_secret(n);
 
-        diffs.push_back(ones_digit(next_n) - ones_digit(n));
+        let prev_ones = n % 10;
+        let next_ones = next_n % 10;
+
+        // who dis?
+        diffs <<= 8;
+        diffs |= (next_ones - prev_ones + 10) as u32;
+
         n = next_n;
 
-        if diffs.len() > 4 {
-            diffs.pop_front();
-        }
-
-        if diffs.get(0) == Some(&c1)
-            && diffs.get(1) == Some(&c2)
-            && diffs.get(2) == Some(&c3)
-            && diffs.get(3) == Some(&c4)
-        {
-            return Some(ones_digit(n));
+        if i >= 3 {
+            map.entry(diffs).or_insert(next_ones);
         }
     }
-    None
-}
-
-fn brute_force(raw_inp: &str) -> i64 {
-    repeat_n((-9..=9).into_iter(), 4)
-        .multi_cartesian_product()
-        .par_bridge()
-        .filter_map(|v| {
-            let c1 = v[0];
-            let c2 = v[1];
-            let c3 = v[2];
-            let c4 = v[3];
-
-            if c1 + c2 < -9
-                || c1 + c2 > 9
-                || c2 + c3 < -9
-                || c2 + c3 > 9
-                || c3 + c4 < -9
-                || c3 + c4 > 9
-                || c1 + c2 + c3 < -9
-                || c1 + c2 + c3 > 9
-                || c2 + c3 + c4 < -9
-                || c2 + c3 + c4 > 9
-                || c1 + c2 + c3 + c4 < -9
-                || c1 + c2 + c3 + c4 > 9
-            {
-                return None;
-            }
-
-            Some(
-                raw_inp
-                    .lines()
-                    .filter_map(|line| line.parse::<i64>().ok())
-                    .filter_map(|n| sell(n, c1, c2, c3, c4))
-                    .sum::<i64>(),
-            )
-        })
-        .max()
-        .unwrap()
+    (n, map)
 }
 
 fn calculate(raw_inp: &str) -> (i64, i64) {
-    let p1 = raw_inp
-        .lines()
+    let (p1, p2) = raw_inp
+        .par_lines()
         .filter_map(|line| line.parse::<i64>().ok())
-        .map(|n| {
-            let mut n = n;
-            for _ in 0..2000 {
-                n = next_secret(n);
-            }
-            n
-        })
-        .sum::<i64>();
+        .map(sell)
+        .reduce(
+            || (0, FxHashMap::with_capacity(5000)),
+            |mut acc, elem| {
+                elem.1.into_iter().for_each(|(k, v)| {
+                    *acc.1.entry(k).or_insert(0) += v;
+                });
+                acc.0 += elem.0;
+                acc
+            },
+        );
 
-    let p2 = brute_force(raw_inp);
-
-    (p1, p2)
+    (p1, p2.into_values().max().expect("no solution?"))
 }
 
 fn main() {
